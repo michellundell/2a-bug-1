85f374d5f4c446256b6ac08b9c7c0168274e1a32
michellundell <michel@lundell.net>
2022-10-23 20:43:20 +0200

    initial commit

---
+++ 2a-bug-1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/*
+** A structure representing a Flight
+*/
+
+struct Flight {
+	char path[10];
+};
+
+
+/* 
+** copy the path into the struct member path 
+*/
+
+void setFlightPath(struct Flight f, const char *path)
+{
+	strcpy(f.path, path );
+}
+
+
+/*
+** Main entry 
+*/
+
+int main(int argc, char **argv)
+{
+	struct Flight flight;
+
+	setFlightPath(flight, "GOT-CPH");
+
+	printf("flight.path=%s\n",flight.path);
+
+	return 0;
+}
